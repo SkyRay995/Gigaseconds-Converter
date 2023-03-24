493c064425253b1f80b4ccc38ba04bb9d11ecc89
Summersong <strokoviv228@mail.ru>
2023-03-24 12:36:34 +0100

    Added Korobla logo

--- MyFrame.java
+++ MyFrame.java
@@ -39,6 +39,8 @@ public class MyFrame extends JFrame implements ActionListener {
     Border border = BorderFactory.createLineBorder(Color.black, 1, true);
 
     MyFrame(){
+        ImageIcon icon = new ImageIcon(getClass().getResource("/Korobla.jpg"));
+        this.setIconImage(icon.getImage());
 
         this.setTitle("Gigaseconds converter");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
